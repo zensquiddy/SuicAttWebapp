97ecdb04ae08be086f3bd1ba636f5b0bc3f752a3
zensquiddy <zensquiddy@gmail.com>
2024-10-10 22:06:54 -0700

    finished looks

--- Webapp.py
+++ Webapp.py
@@ -8,8 +8,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/data')
+def data():
+    return render_template('data.html')
+
+@app.route('/credit')
+def credit():
+    return render_template('credit.html')
 
 if __name__ == '__main__':
     app.run(debug=True) # change to False when running in production
